20cd1dc49db2ca10230cebd3640b367dd3a04604
jaymzee <jaymzee@users.noreply.github.com>
2020-12-08 13:51:51 -0500

    start-sodium comment

--- sodium/start-sodium-serial.bat
+++ sodium/start-sodium-serial.bat
@@ -1,1 +1,2 @@
+REM use something like com0com to connect two serial ports virtually
 qemu-system-x86_64 -drive "format=raw,if=ide,file=C:\Users\James\QEMU VMs\sodium\sodium.img" -serial COM5 -m 256M -accel hax
