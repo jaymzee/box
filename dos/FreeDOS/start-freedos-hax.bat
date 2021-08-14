1ece3cb942d3209675a5bc7276b9048266861c80
jaymzee <jaymzee@users.noreply.github.com>
2021-08-14 12:05:01 -0400

    freedos script updated to new qemu syntax

--- dos/FreeDOS/start-freedos-hax.bat
+++ dos/FreeDOS/start-freedos-hax.bat
@@ -1,1 +1,2 @@
-qemu-system-i386 -drive "format=raw,if=ide,file=C:\Users\James\QEMU VMs\FreeDOS\freedos.img" -m 64M -soundhw pcspk -accel hax
+rem qemu-system-i386 -drive "format=raw,if=ide,file=C:\Users\James\QEMU VMs\FreeDOS\freedos.img" -m 64M -soundhw pcspk -accel hax
+qemu-system-i386 -drive "format=raw,if=ide,file=C:\Users\James\QEMU VMs\FreeDOS\freedos.img" -m 64M -audiodev dsound,id=1 -machine pcspk-audiodev=1 -accel hax
--- dos/FreeDOS/start-freedos.bat
+++ dos/FreeDOS/start-freedos.bat
@@ -1,1 +1,2 @@
-qemu-system-i386 -drive "format=raw,if=ide,file=C:\Users\James\QEMU VMs\FreeDOS\freedos.img" -m 64M -soundhw pcspk
+rem qemu-system-i386 -drive "format=raw,if=ide,file=C:\Users\James\QEMU VMs\FreeDOS\freedos.img" -m 64M -soundhw pcspk
+qemu-system-i386 -drive "format=raw,if=ide,file=C:\Users\James\QEMU VMs\FreeDOS\freedos.img" -m 64M -audiodev dsound,id=1 -machine pcspk-audiodev=1
